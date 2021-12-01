54c743890d1f6da9d0b51ceb9064a488fda051d5
MusaabShalaldeh <musaabsh65@yahoo.com>
2021-12-01 20:56:37 +0200

    fix: fixed calculating totals multipling with the number of rows

--- pages/components/reporttable.js
+++ pages/components/reporttable.js
@@ -2,21 +2,25 @@ import React from "react";
 import { hours } from "../../assets/data";
 
 export default function ReportTable(props) {
-  const [rowTotals, setRowTotals] = React.useState([]);
+  let rowTotals = []
   const calculateTotal = (array) =>
   {
     let total = 0
     for (let i = 0; i < array.length; i++) {
-        total += array[i]        
+      total += array[i]       
     }
     return total
   }
-
+  const resetRowTotals = ()=>{
+    rowTotals = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+  }
+  resetRowTotals()
   return (
-    <section className="w-4/6 m-14 py-2">
+    
+    <section className="w-4/6 py-2 m-14">
       {props.reports.length == 0 && <h2>No Cookie Stands Available</h2>}
       {props.reports.length > 0 && (
-        <table className="mx-auto w-full bg-green-500">
+        <table className="w-full mx-auto bg-green-500">
           <thead>
             <th>Location</th>
             {hours.map((hour) => {
@@ -29,14 +33,9 @@ export default function ReportTable(props) {
                 <tr>
                   {console.log(report)}
                   <td className="border-2 border-gray-800" border-black>{report.location}</td>
-                  {report.hourly_sales.map((sale, i)=>{
-                    if(rowTotals[i] != null)
-                    {
-                        rowTotals[i] = rowTotals[i] + sale
-                    }
-                    else{
-                        rowTotals.push(sale)
-                    }
+                  {report.hourly_sales.map((sale,i)=>{
+                    rowTotals[i] += sale
+                    console.log(`ADDING ${rowTotals[i]} to ${sale} with result OF ${rowTotals[i]+sale}`)
                     return <td className="border-2 border-gray-800">{sale}</td>
                   })}
                   <td className="border-2 border-gray-800">{calculateTotal(report.hourly_sales)}</td>
